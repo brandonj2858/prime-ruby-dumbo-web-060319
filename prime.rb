a81b70311496c1a2d9981181f4913e6ce852e683
Brandon James <brandonj2858@gmail.com>
2019-05-21 23:01:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+  if int < 0 || int == 1 || int == 0
+return false
+else (2 .. int - 1).to_a.all? do |multiples|
+   if int % multiples == 0
+  return false
+  
+end
+return true
+end
+end
+end
